86e318d79a27797a73bcf9209e24e2ef79ee78f1
guswawan <agsn.does6@gmail.com>
2019-07-25 16:32:42 +0700

    make it better again

--- server.js
+++ server.js
@@ -14,18 +14,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 //set mongoose
-var dev_db_url = "mongodb+srv://admin:adminpass@cluster0-f0h1m.mongodb.net/drumkit?retryWrites=true";
+// var dev_db_url = "mongodb+srv://admin:adminpass@cluster0-f0h1m.mongodb.net/drumkit?retryWrites=true";
 
 //connect database
-mongoose.connect(dev_db_url, {
-	useNewUrlParser: true
-});
+// mongoose.connect(dev_db_url, {
+// 	useNewUrlParser: true
+// });
 
-var db = mongoose.connection;
-db.once("open", () => console.log("Connected Database"));
+// var db = mongoose.connection;
+// db.once("open", () => console.log("Connected Database"));
 
 //jika tidak connect database
-db.on("error", console.error.bind(console, "MongoDB connection error"));
+// db.on("error", console.error.bind(console, "MongoDB connection error"));
 
 //API Router
 app.use('/v1', require ('./routes/routes'));
